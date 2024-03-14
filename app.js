9d69232e53b16a11100253ac693b515ee47df55b
ignaarguello <igna.arguello2110@hotmail.com>
2024-03-14 16:44:01 -0300

    Add: cors in app

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 require('dotenv').config();
+const cors = require('cors')
 
 // Conexion con Mongoose
 require('./config/database');
@@ -11,6 +12,7 @@ require('./config/database');
 // Require del modulo index de rutas
 const index_routes = require('./routes/index')
 app.use('/', index_routes)
+app.use(cors())
 
 
 // Metodos de Api
